d1e280eb104f4393c38ad6b8b3bf770acb4223ee
oliver680 <olivermartinez680@gmail.com>
2021-01-21 15:05:45 -0400

    change req to data "create other commint because i forgot".

--- routers/products.js
+++ routers/products.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const service = require("../services/product.service");
 
-router.get("/", async (req, res) => {
-  const { pagenumber, pagesize, name } = req.query;
+router.get("/", async (data, res) => {
+  const { pagenumber, pagesize, name } = data.query;
   const model = await service.get(pagenumber, pagesize, name);
   res.json(model);
 });
-router.post("/", async (req, res) => {
-  const result = await service.post(req);
+router.post("/", async (data, res) => {
+  const result = await service.post(data);
   res.json(result);
 });
 
