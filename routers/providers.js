2496239d1c29cb05cf56b7d866ae7d6dc77a6ac7
oliver680 <olivermartinez680@gmail.com>
2021-01-21 17:45:27 -0400

    change req for data to providers.

--- routers/providers.js
+++ routers/providers.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Service = require("../services/provider.service");
 
-router.get("/", async(req, res) => {
+router.get("/", async(data, res) => {
   const provider =  await Service.get();
   res.json(provider);
 });
 
-router.post("/", async (req, res) => {
-  const result = await Service.post(req);
+router.post("/", async (data, res) => {
+  const result = await Service.post(data);
   res.json(result);
 });
 module.exports = router;
