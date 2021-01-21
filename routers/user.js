b28e26e21552db34fdb23e34a2a8b017d77739fc
oliver680 <olivermartinez680@gmail.com>
2021-01-21 18:29:07 -0400

    change req for data "document user".

--- routers/user.js
+++ routers/user.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const _ = require("lodash");
 const service = require("../services/user.service");
 
-router.get("/", async (req, res) => {
+router.get("/", async (data, res) => {
   const model = await service.get();
   res.json(model);
 });
 
-router.post("/", async (req, res) => {
-  const result = await service.post(req);
+router.post("/", async (data, res) => {
+  const result = await service.post(data);
   res
     .header("x-auth-token", result.token)
     .json(_.pick(result.payload, ["email", "name", "employeeNumber", "_id"]));
