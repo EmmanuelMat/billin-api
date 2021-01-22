9ebe7a0d1484b491a91b796c68f6308e82f9e025
oliver680 <olivermartinez680@gmail.com>
2021-01-21 22:32:21 -0400

    modify document providers.js because it wasn't define.

--- routers/providers.js
+++ routers/providers.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Service = require("../services/provider.service");
 
-router.get("/", async(data, res) => {
+router.get("/", async(req, res) => {
   const provider =  await Service.get();
   res.json(provider);
 });
 
-router.post("/", async (data, res) => {
-  const result = await Service.post(data);
+router.post("/", async (req, res) => {
+  const result = await Service.post(req.body);
   res.json(result);
 });
 module.exports = router;
